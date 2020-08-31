5734b02e9cd945467de9957a3600c9a3fbf9f38c
John Bampton <jbampton@users.noreply.github.com>
2020-08-31 14:08:18 +1000

    Add Go mod and Go sum files.
    Update CONTRIBUTING guide and README.
    
    Lint Go code. Update workflow names.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,34 @@
 # Contributing
+
+To print your Go environment variables run:
+
+```
+go env
+```
+
+From the root of this project install the Golang libraries with:
+
+```
+go get -u ./...
+```
+
+Example run:
+
+```
+go run src/donya.go
+Usage:
+  donya [command]
+
+Available Commands:
+  help        Help about any command
+  install     Installing package(s) in DonyaOS
+
+Flags:
+  -h, --help   help for donya
+
+Use "donya [command] --help" for more information about a command.
+```
+
+- [Go](https://golang.org/) - Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.
+- [revive](https://github.com/mgechev/revive) - 🔥 ~6x faster, stricter, configurable, extensible, and beautiful drop-in replacement for golint.
+- [Cobra](https://github.com/spf13/cobra) - A Commander for modern Go CLI interactions
--- src/donya.go
+++ src/donya.go
@@ -1,24 +1,26 @@
 package main
+
 import (
-  "fmt"
-  "strings"
-  "strconv"
-  "github.com/spf13/cobra"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-  var cmdPrint = &cobra.Command{
-    Use:   "install [name of packages]",
-    Short: "Installing package(s) in DonyaOS",
-    Long: ``,
-    Args: cobra.MinimumNArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-      leng := strconv.Itoa(len(args))
-      fmt.Println("Install "+leng+" package(s): " + strings.Join(args, " "))
-    },
-  }
+	var cmdPrint = &cobra.Command{
+		Use:   "install [name of packages]",
+		Short: "Installing package(s) in DonyaOS",
+		Long:  ``,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			leng := strconv.Itoa(len(args))
+			fmt.Println("Install " + leng + " package(s): " + strings.Join(args, " "))
+		},
+	}
 
-  var rootCmd = &cobra.Command{Use: "donya"}
-  rootCmd.AddCommand(cmdPrint)
-  rootCmd.Execute()
+	var rootCmd = &cobra.Command{Use: "donya"}
+	rootCmd.AddCommand(cmdPrint)
+	rootCmd.Execute()
 }
